fc0a88bac8f59e5bc3cd9ce11330ef076b5c77b5
=Luis Fernando Paganini <luisfernando_paganini@hotmail.com>
2024-05-28 23:48:58 -0300

    fix: i18next not translating with JSON file

--- components/application/CardComponent/index.tsx
+++ components/application/CardComponent/index.tsx
@@ -1,12 +1,12 @@
-import { View, Text, useWindowDimensions } from 'react-native'
-import React from 'react'
+import { useWindowDimensions } from 'react-native'
+import React, { useEffect } from 'react'
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { ThemedView } from '@/components/ThemedView';
 import { Avatar } from 'react-native-paper';
 import { Card, CardTextContent, InfoCardText } from './styles';
 import { ThemedText } from '@/components/ThemedText';
 import IUserInterface from '@/interfaces/IUserInterface';
 import 'intl-pluralrules';
+import { useTranslation } from 'react-i18next';
 
 interface CardComponentProps {
     user: IUserInterface
@@ -18,14 +18,15 @@ export default function CardComponent(props: CardComponentProps) {
 
     const windowWidth = useWindowDimensions().width;
     const avatarSize = windowWidth * 0.2;
-
+    const { t } = useTranslation();
+    
     return (
         <Card colorTheme={colorScheme}>
             <Avatar.Image size={avatarSize} source={{ uri: 'https://randomuser.me/api/portraits/men/12.jpg' }} />
             <CardTextContent>
                 <ThemedText type="defaultSemiBold">{props.user.name + ' ' + props.user.lastName}</ThemedText>
                 <InfoCardText>
-                    <ThemedText type="default">{props.user.sex}</ThemedText>
+                    <ThemedText type="default">{t(props.user.sex)}</ThemedText>
                     <ThemedText type="default">{props.user.dateOfBirth.toLocaleDateString('pt-BR')}</ThemedText>
                 </InfoCardText>
             </CardTextContent>
--- translation/i18n.ts
+++ translation/i18n.ts
@@ -1,22 +1,34 @@
-// i18n.ts
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import HttpBackend from 'i18next-http-backend';
-import 'intl-pluralrules';
+import 'intl-pluralrules'; // Polyfill for Intl.PluralRules
+
+// Arquivos JSON de tradução
+import enTranslation from '../public/locales/en/translation.json';
+import ptTranslation from '../public/locales/pt/translation.json';
+
+const resources = {
+  en: {
+    translation: enTranslation,
+  },
+  pt: {
+    translation: ptTranslation,
+  },
+};
 
 i18n
-    .use(HttpBackend)
-    .use(initReactI18next)
-    .init({
-        fallbackLng: 'en',
-        lng: 'en',
-        interpolation: {
-            escapeValue: false,
-        },
-        backend: {
-            loadPath: '/locales/{{lng}}/{{ns}}.json',
-        },
-    }
-);
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    lng: 'pt', // Default language
+    interpolation: {
+      escapeValue: false, // React already escapes by default
+    },
+    react: {
+      useSuspense: false, // Disable suspense to prevent issues with React Native
+    },
+  })
+  .then(() => console.log('i18n initialized successfully'))
+  .catch((error) => console.error('i18n initialization failed', error));
 
 export default i18n;
